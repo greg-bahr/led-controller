e1e30ebe8205f38bac7c6c6f609ba6f18dfe6afd
greg-bahr <gregsbahr@gmail.com>
2020-10-07 02:01:20 -0500

    over the air updates and wifi

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <Preferences.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <FS.h>
+#include <SPIFFS.h>
 #include "animations/colorwipe/ColorWipeAnimation.h"
 #include "animations/colorfade/ColorFadeAnimation.h"
 #include "animations/fillsolid/FillSolidAnimation.h"
@@ -41,6 +47,63 @@ ColorFadeAnimation colorFadeAnimation(leds, LED_COUNT, *currentColor, currentDel
 void setup()
 {
   Serial.begin(115200);
+  Serial.println();
+
+  SPIFFS.begin();
+  File f = SPIFFS.open("/wifi_credentials.txt", "r");
+  char ssid[30];
+  char password[30];
+
+  int length = f.readBytesUntil('\n', ssid, 30);
+  ssid[length - 1] = '\0';
+  length = f.readBytesUntil('\n', password, 30);
+  password[length] = '\0';
+
+  f.close();
+  SPIFFS.end();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("Connection failed! Restarting...");
+    delay(2000);
+    ESP.restart();
+  }
+
+  ArduinoOTA.setHostname("led-controller");
+
+  ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else // U_SPIFFS
+          type = "filesystem";
+
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() {
+        Serial.println("\nEnd");
+      })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+          Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR)
+          Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR)
+          Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR)
+          Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR)
+          Serial.println("End Failed");
+      });
+
+  ArduinoOTA.begin();
 
   preferences.begin("led-controller", true);
 
@@ -95,6 +158,8 @@ int getColorAsInt()
 
 void loop()
 {
+  ArduinoOTA.handle();
+
   int animationNum = animationCharacteristic->getValue().data()[0];
   if (animationNum != currentAnimation)
   {
